656d22e2f7979d45a637bd9dbc96b110ed1b7e9c
Jack Clackett <jclackett@gmail.com>
2016-08-25 12:17:31 +0200

    update to mailer

--- mailhandler.php
+++ mailhandler.php
@@ -1,5 +1,5 @@
 <?php 
-		$to = "georgeborg00@hotmail.com";
+		$to = "info@jackandgeorge.co";
 		$from = $_POST['email'];
 		$name = $_POST['name'];
 		$subject = "Email from freelance site";
@@ -7,11 +7,6 @@
 		$headers = "From:" . $from;
 
 		mail($to, $subject, $message, $headers);
-		
-		/* 	$conf_subject = "Copy of your email to Green Insights";
-			$conf_message = "Here is a copy of your message you sent to Green Insights. We will get back to you shortly. " . $first_name . "\n\n" . $_POST['message'];
-			$conf_header = "From:" . $to;
-			mail($from, $conf_subject, $conf_message, $conf_header); */
 		
 		echo "Mail Sent (Sender: " . $from . ")";
 ?>
